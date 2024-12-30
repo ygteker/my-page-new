bb9ee7b96d7ac8d22db2a74d22dc599bffde4e13
Yagiz Gunes Teker <gunes@gunesteker.local>
2024-12-30 19:19:22 +0300

    cleanup

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -9,13 +9,17 @@ export default function Resume() {
         <div className="relative h-full w-3/4 mx-auto bg-white p-4 pb-12 space-y-12">
           <Navbar />
           <p className="text-xl font-bold">Business</p>
-          <ResumeEntry dates="Since Ocotber 2023" title="regiocom SE, Berlin" text="Sofware Engineer" />
-          <ResumeEntry dates="October 2022 - September 2023" title="VOQUZ Digital Experts GmbH, Ismaning" text="Sofware Engineer" />
-          <ResumeEntry dates="March 2021 - Febraury 2022" title="amasol AG, Munich" text="IT Consultant" />
-          <ResumeEntry dates="May 2018 - April 2019" title="adabay GmbH, Munich" text="Junior Software Engineer" />
+          <ResumeEntry dates="since 2023" title="regiocom SE, Berlin" text="Sofware Engineer" />
+          <ResumeEntry dates="2022 - 2023" title="VOQUZ Digital Experts GmbH, Ismaning" text="Sofware Engineer" />
+          <ResumeEntry dates="2021 - 2022" title="amasol AG, Munich" text="IT Consultant" />
+          <ResumeEntry dates="2018 - 2019" title="adabay GmbH, Munich" text="Junior Software Engineer" />
           <p className="text-xl font-bold">Education</p>
           <ResumeEntry dates="2021" title="University of Munich" text="Informatik und Statistik in BSc." />
           <ResumeEntry dates="2015" title="Istanbuler Gymnasium" text="Allgemeine Hochschulreife (Abitur)" />
+          <p className="text-xl font-bold">Certifications</p>
+          <ResumeEntry dates="2020" title="English in Computer Science and Information Technology at level C1.1 CEF" text="HTWK Leipzig" />
+          <ResumeEntry dates="2019" title="Consultant for a Day Workshop" text="NTT Data, Munich" />
+          <ResumeEntry dates="2019" title="LMU Entrepreneurship Certificate of Excellence" text="LMU Munich" />
         </div>
       </div>
     </>
